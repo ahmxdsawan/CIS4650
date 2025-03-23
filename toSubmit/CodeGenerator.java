e693ada2560dff12d58e4c8546a2b02dc0b7cabd
Ahmad Sawan <asawan@linux.socs.uoguelph.ca>
2025-03-22 23:03:16 -0400

    Added more func in CodeGen

--- toSubmit/CodeGenerator.java
+++ toSubmit/CodeGenerator.java
@@ -12,12 +12,22 @@ public class CodeGenerator implements AbsynVisitor {
     public static final int AC = 0;   // Accumulator
     public static final int AC1 = 1;  // Secondary accumulator
 
+    // Offsets for stack frame items (adjust these values as needed)
+    public static final int retFO = -1;   // Offset for return address
+    public static final int ofpFO = -2;   // Offset for old frame pointer
+    public static final int paramFO = -3; // Offset for parameters (for output, etc.)
+
     // Instance variables for code generation
     int mainEntry = -1;       // Entry address for main (to be set when main is encountered)
     int globalOffset = 0;     // Next available location in global data memory
     int emitLoc = 0;          // Current instruction memory location
     int highEmitLoc = 0;      // Highest instruction location generated so far
     PrintWriter code;         // Output writer for the assembly code
+
+    // Additional instance variables for I/O and frame management
+    int inputEntry;           // Entry address for the input routine
+    int outputEntry;          // Entry address for the output routine
+    int frameOffset = 0;      // Used to allocate storage in the current stack frame
 
     // Constructor: initialize the output file (e.g., "output.tm")
     public CodeGenerator(String outputFile) {
@@ -155,6 +165,9 @@ public class CodeGenerator implements AbsynVisitor {
     public void visit(FunctionDec exp, int level, boolean isAddr) {
         emitComment("FunctionDec: " + exp.name);
         exp.funaddr = emitLoc; // Record the start address of the function
+        if (exp.name.equals("main")) {
+            mainEntry = exp.funaddr; // Set mainEntry if this is the main function
+        }
         // In a complete implementation, handle parameters and frame setup here.
         if (exp.body != null) {
             exp.body.accept(this, level + 1, false);
@@ -174,9 +187,42 @@ public class CodeGenerator implements AbsynVisitor {
 
     // Stub implementations for the remaining visitor methods:
     @Override
-    public void visit(IfExp exp, int level, boolean isAddr) {
-        emitComment("IfExp not implemented in this skeleton");
+public void visit(IfExp exp, int level, boolean isAddr) {
+    emitComment("Begin if statement");
+    
+    // Generate code for the test condition
+    exp.test.accept(this, level, false);
+    
+    // Save current emit location for backpatching
+    int savedLoc = emitSkip(1);
+    
+    // Generate code for the then part
+    if (exp.thenpart != null) {
+        exp.thenpart.accept(this, level, false);
     }
+    
+    // Save current emit location for backpatching (for else part)
+    int savedLoc2 = emitSkip(1);
+    
+    // Backpatch the jump around the then part
+    int currentLoc = emitLoc;
+    emitBackup(savedLoc);
+    emitRM_Abs("JEQ", AC, currentLoc, "Skip then part if test is false");
+    emitRestore();
+    
+    // Generate code for the else part (if it exists)
+    if (exp.elsepart != null) {
+        exp.elsepart.accept(this, level, false);
+    }
+    
+    // Backpatch the jump around the else part
+    currentLoc = emitLoc;
+    emitBackup(savedLoc2);
+    emitRM_Abs("LDA", PC, currentLoc, "Jump around else part");
+    emitRestore();
+    
+    emitComment("End if statement");
+}
     @Override
     public void visit(TypeExp exp, int level, boolean isAddr) {
         // No code generation needed for type expressions
@@ -188,20 +234,136 @@ public class CodeGenerator implements AbsynVisitor {
     }
     @Override
     public void visit(CallExp exp, int level, boolean isAddr) {
-        emitComment("CallExp: function call " + exp.func);
-        // Set up arguments and generate call instructions here.
+        emitComment("Begin call to " + exp.func);
+        
+        // Special handling for input/output functions
+        if (exp.func.equals("input")) {
+            emitRM_Abs("LDA", PC, inputEntry, "Call input function");
+            return;
+        } else if (exp.func.equals("output")) {
+            // Generate code for the argument
+            if (exp.args != null) {
+                exp.args.accept(this, level, false);
+            }
+            emitRM("ST", AC, frameOffset--, FP, "Store argument");
+            emitRM_Abs("LDA", PC, outputEntry, "Call output function");
+            return;
+        }
+        
+        // Compute and store arguments in reverse order
+        int argCount = 0;
+        int tempOffset = frameOffset;
+        
+        // First pass to compute argument count
+        ExpList args = exp.args;
+        while (args != null) {
+            argCount++;
+            args = args.tail;
+        }
+        
+        // Second pass to generate code for arguments in reverse order
+        args = exp.args;
+        while (args != null) {
+            args.head.accept(this, level, false);
+            emitRM("ST", AC, tempOffset--, FP, "Store argument");
+            args = args.tail;
+        }
+        
+        // Setup for the call
+        emitRM("ST", FP, tempOffset--, FP, "Store old frame pointer");
+        emitRM("LDA", FP, tempOffset, FP, "Update frame pointer");
+        emitRM("LDA", AC, 1, PC, "Save return address");
+        
+        // Jump to function
+        FunctionDec funcDec = (FunctionDec) exp.dtype;
+        emitRM_Abs("LDA", PC, funcDec.funaddr, "Jump to function " + exp.func);
+        
+        // Restore frame pointer
+        emitRM("LD", FP, 0, FP, "Restore frame pointer");
+        
+        emitComment("End call to " + exp.func);
     }
     @Override
     public void visit(WhileExp exp, int level, boolean isAddr) {
-        emitComment("WhileExp not implemented in this skeleton");
+        emitComment("Begin while statement");
+        
+        // Save the location of the test
+        int savedLoc1 = emitLoc;
+        
+        // Generate code for the test condition
+        exp.test.accept(this, level, false);
+        
+        // Save current emit location for backpatching
+        int savedLoc2 = emitSkip(1);
+        
+        // Generate code for the body
+        if (exp.body != null) {
+            exp.body.accept(this, level, false);
+        }
+        
+        // Generate code to jump back to the test
+        emitRM_Abs("LDA", PC, savedLoc1, "Jump back to while test");
+        
+        // Backpatch the jump around the body
+        int currentLoc = emitLoc;
+        emitBackup(savedLoc2);
+        emitRM_Abs("JEQ", AC, currentLoc, "Skip while body if test is false");
+        emitRestore();
+        
+        emitComment("End while statement");
     }
     @Override
     public void visit(ReturnExp exp, int level, boolean isAddr) {
-        emitComment("ReturnExp not implemented in this skeleton");
+        emitComment("Begin return statement");
+        
+        // Generate code for the return expression (if any)
+        if (exp.exp != null) {
+            exp.exp.accept(this, level, false);
+        }
+        
+        // Return to caller
+        emitRM("LD", PC, retFO, FP, "Return to caller");
+        
+        emitComment("End return statement");
     }
     @Override
     public void visit(IndexVar var, int level, boolean isAddr) {
-        emitComment("IndexVar not implemented in this skeleton");
+        emitComment("Begin array indexing for " + var.name);
+        
+        // Load base address of the array
+        int base = (var.nestLevel == 0 ? GP : FP);
+        emitRM("LDA", AC, var.offset, base, "Load base address of array " + var.name);
+        
+        // Save the base address temporarily
+        emitRM("ST", AC, frameOffset--, FP, "Save base address");
+        
+        // Compute the index
+        var.index.accept(this, level, false);
+        
+        // Load the base address back
+        emitRM("LD", AC1, ++frameOffset, FP, "Restore base address");
+        
+        // Check for index out of bounds (below 0)
+        emitRM("JLT", AC, 2, PC, "Jump if index < 0");
+        
+        // Load the array size
+        emitRM("LD", AC1, -1, AC1, "Load array size");
+        
+        // Check for index out of bounds (above size)
+        emitRM("SUB", AC1, AC1, AC, "Compute size - index");
+        emitRM("JLE", AC1, 2, PC, "Jump if index >= size");
+        
+        // Compute the indexed address (base address + index + 1)
+        emitRM("LD", AC1, frameOffset, FP, "Reload base address");
+        emitRM("LDA", AC, 1, AC, "Add 1 to index (to skip size)");
+        emitRM("ADD", AC, AC1, AC, "Compute address = base + index + 1");
+        
+        if (!isAddr) {
+            // Load the value at the computed address
+            emitRM("LD", AC, 0, AC, "Load value from array");
+        }
+        
+        emitComment("End array indexing for " + var.name);
     }
     @Override
     public void visit(BoolExp exp, int level, boolean isAddr) {
@@ -216,20 +378,58 @@ public class CodeGenerator implements AbsynVisitor {
        Method to trigger code generation from an AST
        --------------------------------------------------- */
     public void generate(Absyn ast) {
-        // Emit prelude instructions (initialization)
-        emitComment("Prelude");
-        emitRM("LD", GP, 0, 0, "Load gp with max address");
-        emitRM("LDA", FP, 0, GP, "Set fp = gp");
-        emitRM("ST", AC, 0, AC, "Clear value at location 0");
-
-        // Generate code for the AST; pass false to indicate we want the computed value
-        ast.accept(this, 0, false);
-
-        // Emit finale instructions (halt execution)
-        emitComment("Finale");
-        emitRO("HALT", AC, 0, 0, "Stop execution");
-
-        // Close the output file
-        code.close();
+    // Initialize global variables
+    mainEntry = -1;
+    globalOffset = 0;
+    
+    // Emit prelude instructions
+    emitComment("Prelude");
+    emitRM("LD", GP, 0, 0, "Load gp with max address");
+    emitRM("LDA", FP, 0, GP, "Set fp = gp");
+    emitRM("ST", AC, 0, AC, "Clear value at location 0");
+    
+    // Save location to jump around I/O routines
+    int savedLoc = emitSkip(1);
+    
+    // Generate I/O routines
+    emitComment("Code for input routine");
+    inputEntry = emitLoc;
+    emitRM("ST", AC, retFO, FP, "Store return address");
+    emitRO("IN", AC, 0, 0, "Input integer");
+    emitRM("LD", PC, retFO, FP, "Return to caller");
+    
+    emitComment("Code for output routine");
+    outputEntry = emitLoc;
+    emitRM("ST", AC, retFO, FP, "Store return address");
+    emitRM("LD", AC, paramFO, FP, "Load output value");
+    emitRO("OUT", AC, 0, 0, "Output integer");
+    emitRM("LD", PC, retFO, FP, "Return to caller");
+    
+    // Backpatch jump around I/O routines
+    int currentLoc = emitLoc;
+    emitBackup(savedLoc);
+    emitRM_Abs("LDA", PC, currentLoc, "Jump around I/O routines");
+    emitRestore();
+    
+    // Generate code for the AST; pass false to indicate we want the computed value
+    ast.accept(this, 0, false);
+    
+    // Check if main function was found
+    if (mainEntry == -1) {
+        System.err.println("Error: main function not found");
+        return;
+    }
+    
+    // Emit finale instructions
+    emitComment("Finale");
+    emitRM("ST", FP, globalOffset+ofpFO, FP, "Push old frame pointer");
+    emitRM("LDA", FP, globalOffset, FP, "Push new frame");
+    emitRM("LDA", AC, 1, PC, "Load return address");
+    emitRM_Abs("LDA", PC, mainEntry, "Jump to main");
+    emitRM("LD", FP, ofpFO, FP, "Pop frame");
+    emitRO("HALT", 0, 0, 0, "Stop execution");
+    
+    // Close the output file
+    code.close();
     }
 }
